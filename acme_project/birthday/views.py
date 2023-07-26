c9048e95e52c899fbda93911cbf3f5284956c81e
Михаил Ренжин <renzhin@gmail.com>
2023-07-26 17:48:42 +0300

    Добавить новую страницу birthday_list с перечнем записей, прописав ее в urls, views и шаблоне меню

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+# Импортируем модель дней рождения
+from .models import Birthday
 
 
 def birthday(request):
@@ -12,6 +14,7 @@ def birthday(request):
     context = {'form': form}
     # Если форма валидна...
     if form.is_valid():
+        form.save()
         # ...вызовем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
@@ -20,3 +23,11 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
