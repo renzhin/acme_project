68984b2c866858c7dcf5d8d8d521052b47282d25
Михаил Ренжин <renzhin@gmail.com>
2023-07-27 22:16:44 +0300

    Добавить валидацию формы по возрасту, имени и фамилии

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+
+# Из модуля models импортируем модель Birthday...
+from .models import Birthday
+
+
+# ...и регистрируем её в админке:
+admin.site.register(Birthday)
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,8 +1,13 @@
 # birthday/forms.py
 from django import forms
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
 
 # Импортируем класс модели Birthday.
 from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
@@ -18,3 +23,20 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
