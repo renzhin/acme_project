32bc71f24ee1bd57151895af91456a9fb7b8f864
Михаил Ренжин <renzhin@gmail.com>
2023-07-27 14:22:09 +0300

    Добавить удаление объекта через форму

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,5 @@
 # birthday/views.py
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
 # Импортируем модель дней рождения
@@ -40,3 +40,20 @@ def birthday_list(request):
     # Передаём их в контекст шаблона.
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
