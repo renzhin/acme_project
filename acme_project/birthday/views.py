f975feddc3f9e89658f1cbea54a0f028a9cccfd5
Михаил Ренжин <renzhin@gmail.com>
2023-07-29 08:34:37 +0300

    применить для views CBV и миксины

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,6 @@
 # birthday/views.py
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 # Импортируем класс пагинатора.
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
@@ -8,6 +10,38 @@ from .forms import BirthdayForm
 from .models import Birthday
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
 
 
 # Добавим опциональный параметр pk.
