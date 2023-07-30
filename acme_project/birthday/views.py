d994f6bbd04ddfe8892152f3a797a13d17bcf98b
Михаил Ренжин <renzhin@gmail.com>
2023-07-30 16:01:07 +0300

    Оптимизировать код после внесения BirthdayDetailView

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,7 +2,6 @@
 from django.views.generic import (
     CreateView, DetailView, DeleteView, ListView, UpdateView
 )
-from django.urls import reverse_lazy
 
 
 from .forms import BirthdayForm
@@ -14,28 +13,21 @@ from .utils import calculate_birthday_countdown
 
 class BirthdayMixin:
     model = Birthday
-    success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
+class BirthdayCreateView(BirthdayMixin, CreateView):
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
 
 
-class BirthdayDetailView(DetailView):
-    model = Birthday
+class BirthdayDetailView(BirthdayMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         # Получаем словарь контекста:
