5581843c648eeae71d16191a0b9a1f8b2fdf911b
Михаил Ренжин <renzhin@gmail.com>
2023-08-02 19:39:13 +0300

    Добавить модель Teg в проект и вывести инфу по тегам на birthday_list.html

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 
 # Из модуля models импортируем модель Birthday...
-from .models import Birthday
+from .models import Birthday, Tag
 
 
 # ...и регистрируем её в админке:
+admin.site.register(Tag)
 admin.site.register(Birthday)
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -12,6 +12,14 @@ from .validators import real_age
 User = get_user_model()
 
 
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    # Переопределяем метод:
+    def __str__(self):
+        return self.tag
+
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
@@ -25,6 +33,16 @@ class Birthday(models.Model):
     author = models.ForeignKey(
         User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
     )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
+    )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
 
     class Meta:
         constraints = (
@@ -46,8 +64,4 @@ class Congratulation(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('created_at',) 
-
-    def get_absolute_url(self):
-        # С помощью функции reverse() возвращаем URL объекта.
-        return reverse('birthday:detail', kwargs={'pk': self.pk})
+        ordering = ('created_at',)
